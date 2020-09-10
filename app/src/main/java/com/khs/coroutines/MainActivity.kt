f6681cd8773b165f0f5128a9d27c426eca1ae104
happiness <melow2@naver.com>
2020-09-10 10:33:37 +0900

    modify

--- app/src/main/java/com/khs/coroutines/MainActivity.kt
+++ app/src/main/java/com/khs/coroutines/MainActivity.kt
@@ -3,13 +3,15 @@ package com.khs.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,17 +20,42 @@ class MainActivity : AppCompatActivity() {
             tvCount.text = count++.toString()
 
         }
+
         btnDownloadUserData.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
+/*            CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
+            }*/
+            CoroutineScope(Main).launch {
+                Log.i("MyTag", "Calculation started..")
+                val stock1 = async(IO) { getStock1() }
+                val stock2 = async(IO) { getStock2() }
+                val total = stock1.await()+ stock2.await()
+                Log.i("MyTag", "Total is $total")
+                Toast.makeText(applicationContext,"Total is $total",Toast.LENGTH_SHORT).show()
             }
-
         }
+
     }
 
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+            delay(3000)
         }
     }
+
+    private suspend fun getStock1(): Int {
+        delay(10000)
+        Log.i("MyTag", "stock 1 returned ")
+        return 55000
+    }
+
+    private suspend fun getStock2(): Int {
+        delay(8000)
+        Log.i("MyTag", "stock 2 returned ")
+        return 35000
+    }
+
 }
